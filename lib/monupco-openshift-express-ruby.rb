274694c25824dcf9317ef6e58a82d81165a6865b
Zaro <zarrro@gmail.com>
2012-04-10 14:45:04 +0300

    Fix pkg_type field

--- lib/monupco-openshift-express-ruby.rb
+++ lib/monupco-openshift-express-ruby.rb
@@ -10,7 +10,6 @@ module Monupco
 		  'app_type' => 'Ruby',
 		  'app_url'  => "http://#{ENV['OPENSHIFT_APP_DNS']}/",
 		  'app_vendor' => 0,
-		  'pkg_type' => 2,
 		  'url' => ENV['MONUPCO_REGISTER_URL'],
     	})
     end
--- lib/monupco-openshift-express-ruby/version.rb
+++ lib/monupco-openshift-express-ruby/version.rb
@@ -1,7 +1,7 @@
 module Monupco
   module Openshift
     class Express
-	    VERSION = "0.0.6"
+	    VERSION = "0.0.7"
     end
   end
 end
